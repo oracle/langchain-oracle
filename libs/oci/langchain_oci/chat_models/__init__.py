f661e9529d82325e87758f437e56f0a66a0fcff7
Qiu Qin <qiu.qin@oracle.com>
2025-09-18 17:04:48 -0400

    Update OCI model deployment clients to inherit from OpenAI clients.

--- libs/oci/langchain_oci/chat_models/__init__.py
+++ libs/oci/langchain_oci/chat_models/__init__.py
@@ -1,16 +1,26 @@
 # Copyright (c) 2025 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-from langchain_oci.chat_models.oci_data_science import (
-    ChatOCIModelDeployment,
-    ChatOCIModelDeploymentTGI,
-    ChatOCIModelDeploymentVLLM,
-)
 from langchain_oci.chat_models.oci_generative_ai import ChatOCIGenAI
+
+try:
+    from langchain_oci.chat_models.oci_data_science import ChatOCIModelDeployment
+
+except ModuleNotFoundError as ex:
+    if ex.name == "langchain_openai":
+        message = (
+            "No module named langchain_openai. "
+            "Please install it with `pip install langchain_openai`"
+        )
+    else:
+        message = ex.msg
+
+    class ChatOCIModelDeployment:
+        def __init__(self, *args, **kwargs):
+            raise ModuleNotFoundError(message)
+
 
 __all__ = [
     "ChatOCIGenAI",
     "ChatOCIModelDeployment",
-    "ChatOCIModelDeploymentTGI",
-    "ChatOCIModelDeploymentVLLM",
 ]
--- libs/oci/langchain_oci/llms/__init__.py
+++ libs/oci/langchain_oci/llms/__init__.py
@@ -1,19 +1,35 @@
 # Copyright (c) 2025 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-from langchain_oci.llms.oci_data_science_model_deployment_endpoint import (
-    BaseOCIModelDeployment,
-    OCIModelDeploymentLLM,
-    OCIModelDeploymentTGI,
-    OCIModelDeploymentVLLM,
-)
+
 from langchain_oci.llms.oci_generative_ai import OCIGenAI, OCIGenAIBase
+
+try:
+    from langchain_oci.llms.oci_data_science_model_deployment_endpoint import (
+        BaseOCIModelDeployment,
+        OCIModelDeploymentLLM,
+    )
+except ModuleNotFoundError as ex:
+
+    if ex.name == "langchain_openai":
+        message = (
+            "No module named langchain_openai. "
+            "Please install it with `pip install langchain_openai`"
+        )
+    else:
+        message = ex.msg
+
+    class BaseOCIModelDeployment:
+        def __init__(self, *args, **kwargs):
+            raise ModuleNotFoundError(message)
+
+    class OCIModelDeploymentLLM(BaseOCIModelDeployment):
+        pass
+
 
 __all__ = [
     "OCIGenAIBase",
     "OCIGenAI",
     "BaseOCIModelDeployment",
     "OCIModelDeploymentLLM",
-    "OCIModelDeploymentTGI",
-    "OCIModelDeploymentVLLM",
 ]
