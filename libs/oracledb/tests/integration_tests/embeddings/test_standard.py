9ccf755553a03331c345b299496a54d98cbcf2c6
Elif Sema Balcioglu <elif.balcioglu@oracle.com>
2025-09-19 13:55:05 +0000

    Add standard tests

---
+++ libs/oracledb/tests/integration_tests/embeddings/test_standard.py
@@ -0,0 +1,32 @@
+from typing import Type
+
+import oracledb
+import pytest
+from langchain_tests.integration_tests import EmbeddingsIntegrationTests
+
+from langchain_oracledb import OracleEmbeddings
+
+username = ""
+password = ""
+dsn = ""
+
+try:
+    oracledb.connect(user=username, password=password, dsn=dsn)
+except Exception as e:
+    pytest.skip(
+        allow_module_level=True,
+        reason=f"Database connection failed: {e}, skipping tests.",
+    )
+
+
+class TestOracleEmbeddingsModelIntegration(EmbeddingsIntegrationTests):
+    @property
+    def embeddings_class(self) -> Type[OracleEmbeddings]:
+        # Return the embeddings model class to test here
+        return OracleEmbeddings
+
+    @property
+    def embedding_model_params(self) -> dict:
+        # Return initialization parameters for the model.
+        conn = oracledb.connect(user=username, password=password, dsn=dsn)
+        return {"conn": conn, "params": {"provider": "database", "model": "allminilm"}}
