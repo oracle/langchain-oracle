af31374e162821ffc1bfa0e508593a7439a061e2
paxiaatucsdedu <paxia@ucsd.edu>
2025-11-06 15:36:33 -0800

    Fix make lint error
    Fix make lint error:
    tests/unit_tests/llms/test_oci_model_deployment_endpoint.py:60: error: Missing named argument "response" for "HTTPError"  [call-arg]
    Found 1 error in 1 file (checked 29 source files)

--- libs/oci/tests/unit_tests/llms/test_oci_model_deployment_endpoint.py
+++ libs/oci/tests/unit_tests/llms/test_oci_model_deployment_endpoint.py
@@ -57,7 +57,7 @@ class MockResponse:
     def raise_for_status(self) -> None:
         """Mocked raise for status."""
         if 400 <= self.status_code < 600:
-            raise HTTPError()
+            raise HTTPError(response=self)  # type: ignore[arg-type]
 
     def json(self) -> Dict:
         """Returns mocked json data."""
