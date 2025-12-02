b3f167277fcc68ccc878a7a6344f6e1039acf395
sudarshan12s <sudarshan12s@gmail.com>
2025-12-02 22:43:30 +0530

    - Add lint, packaging rules

---
+++ libs/js/oracledb/langchain-oracledb/eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest",
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+        "no-redeclare": "off",
+    },
+  },
+];
+
