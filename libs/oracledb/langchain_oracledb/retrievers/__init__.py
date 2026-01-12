8e23f93f61f2cab1c1877128ddc95a89cb767e30
Elif Sema Balcioglu <elif.balcioglu@oracle.com>
2026-01-12 21:03:30 +0100

    Add Hybrid and Text Search to langchain-oracledb (#92)
    * Refactor vector code
    
    * Add hybrid retriever
    
    * Add more hybrid search options
    
    * Add OracleTextSearchRetriever
    
    * Fix text search bug
    
    * Change text search to match hybrid approach

---
+++ libs/oracledb/langchain_oracledb/retrievers/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2025 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+from langchain_oracledb.retrievers.hybrid_search import (
+    OracleHybridSearchRetriever,
+    OracleVectorizerPreference,
+)
+from langchain_oracledb.retrievers.text_search import (
+    OracleTextSearchRetriever,
+)
+
+__all__ = [
+    "OracleVectorizerPreference",
+    "OracleHybridSearchRetriever",
+    "OracleTextSearchRetriever",
+]
