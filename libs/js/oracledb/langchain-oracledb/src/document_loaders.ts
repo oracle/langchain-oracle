718f2b36f22248b8d33358e2435b9c71204df062
sudarshan12s <sudarshan12s@gmail.com>
2026-01-30 20:55:37 +0530

    - keep this change in differnt PR

--- libs/js/oracledb/langchain-oracledb/src/document_loaders.ts
+++ libs/js/oracledb/langchain-oracledb/src/document_loaders.ts
@@ -141,7 +141,7 @@ export class OracleDocLoader extends BaseDocumentLoader {
                             sys.dbms_assert.qualified_sql_name(:qn) from dual`;
       const binds = [col, qn];
       await this.conn.execute(sql, binds);
-    } catch (_error) {
+    } catch (error) {
       throw new Error("Invalid owner, table, or column name");
     }
 
